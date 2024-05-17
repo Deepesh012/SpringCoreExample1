a4e488cf5e63ef59317d92b01aac6cc4becee404
Deepesh Thakur <128272082+Deepesh012@users.noreply.github.com>
2024-05-17 16:38:34 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,53 @@
+package com.springcore;
+
+public class Student {
+
+	private int studentID;
+	private String studentName;
+	private String studentAddress;
+	
+	
+	public int getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	
+	
+	public Student(int studentID, String studentName, String studentAddress) {
+		super();
+		this.studentID = studentID;
+		this.studentName = studentName;
+		this.studentAddress = studentAddress;
+	}
+	
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [studentID=" + studentID + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+				+ "]";
+	}
+	
+	
+	
+	
+}
